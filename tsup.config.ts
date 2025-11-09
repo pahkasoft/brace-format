0b8b108b716e033a530f3f2b6642e76e76ab813a
S <77819265+pahkasoft@users.noreply.github.com>
2025-11-09 20:09:14 +0200

    chore: updated build system.

--- tsup.config.ts
+++ tsup.config.ts
@@ -20,10 +20,16 @@ console.log("tsup: LIB_NAME  = " + LIB_NAME);
 console.log("tsup: DIST_PATH = " + DIST_PATH);
 
 const ConfigEntries: { entry: Record<string, string>, format: Format }[] = [
+    // esm bundle
     { entry: { 'index': 'src/index.ts' }, format: 'esm' },
+    // cjs bundle
     { entry: { 'index': 'src/index.ts' }, format: 'cjs' },
+    // global bundle (kept for legacy support, renamed to iife).
     { entry: { 'index.global': 'src/index.ts' }, format: 'iife' },
-    { entry: { 'index.polyfilled.global': 'src/index.polyfilled.ts' }, format: 'iife' }
+    // iife bundle
+    { entry: { 'index.iife': 'src/index.ts' }, format: 'iife' },
+    // polyfilled iife bundle
+    { entry: { 'index.polyfilled.iife': 'src/index.polyfilled.ts' }, format: 'iife' }
 ];
 
 const TsupConfig: Options[] = ConfigEntries.map((cfg, cfgId) => {
@@ -31,16 +37,17 @@ const TsupConfig: Options[] = ConfigEntries.map((cfg, cfgId) => {
     return {
         entry: cfg.entry,
         outDir: DIST_PATH,
-        target: 'es5',
+        target: cfg.format === 'esm' ? 'es2015' : 'es5', // Let'skeep it how it was.
         format: [cfg.format],
         dts: cfg.format === 'cjs',
         globalName: isIIFE ? LIB_NAME : undefined,
         sourcemap: !isIIFE,
+        minify: isIIFE,
         clean: cfgId === 0,
         external: isIIFE ? undefined : ['jsbi'],
         noExternal: isIIFE ? ['jsbi'] : undefined,
         banner: {
-            js: JSON.stringify(`/* ${LIB_NAME} v${pkg.version} (${cfg.format}) | (c) 2025 PahkaSoft | MIT License | Includes JSBI (Apache License 2.0) */`)
+            js: `/* ${LIB_NAME} v${pkg.version} (${cfg.format}) | (c) 2025 PahkaSoft | MIT License | Includes JSBI (Apache License 2.0) */`
         },
         define: {
             __LIB_INFO__: JSON.stringify(`${LIB_NAME} v${pkg.version} (${cfg.format})`)
