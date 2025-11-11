b63b2512aba7c4030702dc5d33b58bedf56b9fdc
PahkaSoft <77819265+pahkasoft@users.noreply.github.com>
2025-11-11 17:57:37 +0200

    chore: retored esm target to es2015.

--- tsup.config.ts
+++ tsup.config.ts
@@ -38,7 +38,7 @@ const TsupConfig: Options[] = ConfigEntries.map((cfg, cfgId) => {
     return {
         entry: cfg.entry,
         outDir: DIST_PATH,
-        target: 'es5',
+        target: cfg.format === 'esm' ? 'es2015' : 'es5', // Let's keep how it was.
         format: [cfg.format],
         dts: cfg.format === 'cjs',
         globalName: isIIFE ? LIB_NAME : undefined,
